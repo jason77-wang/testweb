a6d7e6252c79c1157e8d8e4a3559583c2f6ab52d
jason77-wang <jingdou20@126.com>
2017-09-25 09:37:02 +0800

    add admin page

--- Article/admin.py
+++ Article/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from Article.models import Test
+from Article.models import Tag
+from Article.models import Contact
+ 
+# Register your models here.
+#admin.site.register([Test, Contact, Tag])
 
 # Register your models here.
+class TagInline(admin.TabularInline):
+    model = Tag
+ 
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name','age', 'email') # list
+    search_fields = ('name',)
+    inlines = [TagInline]  # Inline
+    fieldsets = (
+        ['Main',{
+            'fields':('name','email'),
+        }],
+        ['Advance',{
+            'classes': ('collapse',),
+            'fields': ('age',),
+        }]
+ 
+    )
+
+admin.site.register(Contact, ContactAdmin)
+admin.site.register([Test, Tag])
