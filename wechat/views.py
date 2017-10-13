68c586623a4e08908457d848e1165985a0881e19
Hui Wang <hui.wang@canonical.com>
2017-10-13 06:48:13 +0000

    wechat: auto reply chinese

--- wechat/views.py
+++ wechat/views.py
@@ -1,3 +1,5 @@
+# coding: UTF-8
+
 from django.shortcuts import render
 from django.http import HttpResponse
 import hashlib
@@ -11,16 +13,13 @@ WEIXIN_TOKEN = 'xiaochuchinese'
 @csrf_exempt
 # Create your views here.
 def main(request):
-    print request
+
     if request.method == "GET":
         signature = request.GET.get("signature", None)
-        print signature
         timestamp = request.GET.get("timestamp", None)
-        print timestamp
         nonce = request.GET.get("nonce", None)
-        print nonce
         echostr = request.GET.get("echostr", None)
-        print echostr
+
         token = WEIXIN_TOKEN
         tmp_list = [token, timestamp, nonce]
         tmp_list.sort()
@@ -32,17 +31,23 @@ def main(request):
             return HttpResponse("weixin  index")
         
     elif request.method == "POST":
+        tpl = u'''<xml><ToUserName><![CDATA[%s]]></ToUserName>
+<FromUserName><![CDATA[%s]]></FromUserName>
+<CreateTime>1507874304</CreateTime>
+<MsgType><![CDATA[text]]></MsgType>
+<Content><![CDATA[%s]]></Content>
+<MsgId>6476270822591158287</MsgId>
+</xml>'''
+
         xml_str = smart_str(request.body)
         request_xml = etree.fromstring(xml_str)
-        response_xml = request_xml
         c1 = request_xml[0]
         c2 = request_xml[1]
-        tmp = c1.text
+        txt = request_xml[4]
 
-        response_xml[0].text = c2.text
-        response_xml[1].text = tmp
-        response_xml[4].text = "welcome!"
-        print etree.tostring(response_xml)
-        return HttpResponse(etree.tostring(response_xml))
+	smsg = tpl % (c2.text, c1.text, u"您发的内容为: "+txt.text)
+#	smsg = tpl % (c2.text, c1.text, u"字符编码声明")
+#        print smsg
+        return HttpResponse(smsg)
 
 
